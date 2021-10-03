8d3a35be1f93219c2b580e1ff27eeb91cbd5891f
Soojin <soojl1005@naver.com>
2021-10-03 19:22:31 +0900

    css 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,6 @@
 import "./app.css";
+import React, { useState } from "react";
+
 import About from "./components/about/about";
 import Header from "./components/header/header";
 import Home from "./components/home/home";
@@ -7,9 +9,20 @@ import "aos/dist/aos.css";
 import Skills from "./components/skills/skills";
 import Projects from "./components/projects/projects";
 import Contact from "./components/contact/contact";
-import { useEffect, useState } from "react";
+import { FaArrowCircleUp } from "react-icons/fa";
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const scrollY = window.scrollY;
+    if (scrollY > 0) {
+      setVisible(true);
+    }
+    window.scrollTo(0, 0);
+  };
+
   return (
     <div className="portfolio">
       <Header />
@@ -18,6 +31,12 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <button
+        className={visible ? "button_scrollUp" : "button_scrollUp_invisible"}
+        onClick={handleClick}
+      >
+        <FaArrowCircleUp />
+      </button>
       <footer className="footer">
         Copyright 2021. Lee Soojin All Rights Reserved
       </footer>
